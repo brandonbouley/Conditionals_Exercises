f49d7812b81596f2608370fd5d14b1763f1cef64
brandonbouley <brandonbouley@billericak12.com>
2019-11-01 11:34:09 -0400

    gvggf

--- src/IDK.java
+++ src/IDK.java
@@ -1,5 +1,8 @@
+import java.awt.*;
+
 public class IDK {
-    public double totalWages(double hours, double rate){
+    //import java.awt.Color
+    public static double totalWages(double hours, double rate){
         double overtime=hours-40;
         double overrate=rate*1.5;
 
@@ -9,52 +12,124 @@ public class IDK {
             return ((40*rate)+(overrate*overtime));
     }
 
-    public boolean isLeapYear(int year){
-        //if (year/4==)
+    public static boolean isLeapYear(int year) {
+        if (year % 4 == 0 && (year%100!=0 || year%400==0))
+            return true;
+        else
+            return false;
+    }
+    public static boolean isLater(int month1, int day1, int year1, int month2, int day2, int year2){
+        if (year1>year2)
+            return true;
+        else if (month1>month2)
+            return true;
+        else if (day1>day2)
+            return true;
+        else
+            return false;
+
+    }
+    public static Color bestMatch(int r, int g, int b){
+        if (r==g && g==b)
+            return Color.GRAY;
+        else if (r>g && r>b)
+            return Color.RED;
+        else if (g>b && g>r)
+            return Color.GREEN;
+        else if (b>r && b>g)
+            return Color.BLUE;
+        else if (r==g && r>b)
+            return Color.YELLOW;
+        else if (r==b && r>g)
+            return Color.MAGENTA;
+        else
+            return Color.CYAN;
+
+
+
+
+    }
+
+    public static int findBestFit (int size1, int size2, int space){
+        int value;
+        if  (size1+size2<=space)
+            value=3;
+        else if (size1+size2>space && size1<space && size1>size2)
+            value=1;
+        else if (size1+size2>space && size2<space && (size2>size1 || size2>space))
+            value=2;
+        else if (size1>space && size2>space)
+            value=0;
+
+        else
+            value=4;
+        return value;
     }
 
 
 
 
-    /* double wages = totalWages(45, 12.5);
-System.out.println(&quot;Wages for 45 hours at $12.50 &quot; + wages);
-    wages = totalWages(30, 10);
-System.out.println(&quot;Wages for 30 hours at $10.00 &quot; + wages);
-// leap year
-System.out.println(&quot;2000 &quot; + isLeapYear(2000));
-System.out.println(&quot;2004 &quot; + isLeapYear(2004));
-System.out.println(&quot;2003 &quot; + isLeapYear(2003));
-System.out.println(&quot;2100 &quot; + isLeapYear(2100));
-// is later
-System.out.println(&quot;1/2/2010 is later than 1/2/2011 &quot; + isLater(1,2,2010,
-                                                                                  1,2,2011));
-System.out.println(&quot;1/2/2011 is later than 1/2/2010 &quot; + isLater(1,2,2011,
-                                                                                  1,2,2010));
-System.out.println(&quot;1/2/2010 is later than 3/2/2010 &quot; + isLater(1,2,2010,
-                                                                                  3,2,2010));
-System.out.println(&quot;3/2/2010 is later than 1/2/2010 &quot; + isLater(3,2,2010,
-                                                                                  1,2,2010));
-System.out.println(&quot;1/3/2010 is later than 1/2/2010 &quot; + isLater(1,3,2010,
-                                                                                  1,2,2010));
-System.out.println(&quot;1/2/2010 is later than 1/3/2011 &quot; + isLater(1,2,2010,
-                                                                                  1,3,2010));
-System.out.println(&quot;1/2/2010 is later than 1/2/2010 &quot; + isLater(1,2,2010,
-                                                                                  1,2,2010));
-// Best match
-System.out.println(&quot;Best match 2 3 4 &quot; + bestMatch(2,3,4));
-System.out.println(&quot;Best match 4 3 3 &quot; + bestMatch(4,3,3));
-System.out.println(&quot;Best match 3 8 4 &quot; + bestMatch(3,8,4));
-System.out.println(&quot;Best match 4 3 4 &quot; + bestMatch(4,3,4));
-System.out.println(&quot;Best match 2 4 4 &quot; + bestMatch(2,4,4));
-System.out.println(&quot;Best match 8 8 4 &quot; + bestMatch(8,8,4));
 
-System.out.println(&quot;Best match 4 4 4 &quot; + bestMatch(4,4,4));
-// Best Fit
-System.out.println(&quot;Find Best fit 2 3 6 is &quot; + findBestFit(2,3,6));
-System.out.println(&quot;Find Best fit 4 3 6 is &quot; + findBestFit(4,3,6));
-System.out.println(&quot;Find Best fit 3 4 6 is &quot; + findBestFit(3,4,6));
-System.out.println(&quot;Find Best fit 2 3 1 is &quot; + findBestFit(2,3,1));
-System.out.println(&quot;Find Best fit 6 3 4 is &quot; + findBestFit(6,3,4));
-System.out.println(&quot;Find Best fit 3 6 4 is &quot; + findBestFit(3,6,4));
- */
-}
+
+
+
+
+
+
+
+
+
+    public static void main(String [] args) {
+        //total wages
+        double wages = totalWages(45, 12.5);
+        System.out.println("Wages for 45 hours at $12.50 " + wages);
+        wages = totalWages(30, 10);
+        System.out.println("Wages for 30 hours at $10.00 " + wages);
+
+        // leap year
+        System.out.println("2000 " + isLeapYear(2000));
+        System.out.println("2004 " + isLeapYear(2004));
+        System.out.println("2003 " + isLeapYear(2003));
+        System.out.println("2100 " + isLeapYear(2100));
+
+        // is later
+        System.out.println("1/2/2010 is later than 1/2/2011 " + isLater(1,2,2010, 1,2,2011));
+        System.out.println("1/2/2011 is later than 1/2/2010 " + isLater(1,2,2011, 1,2,2010));
+        System.out.println("1/2/2010 is later than 3/2/2010 " + isLater(1,2,2010, 3,2,2010));
+        System.out.println("3/2/2010 is later than 1/2/2010 " + isLater(3,2,2010, 1,2,2010));
+        System.out.println("1/3/2010 is later than 1/2/2010 " + isLater(1,3,2010, 1,2,2010));
+        System.out.println("1/2/2010 is later than 1/3/2011 " + isLater(1,2,2010, 1,3,2010));
+        System.out.println("1/2/2010 is later than 1/2/2010 " + isLater(1,2,2010, 1,2,2010));
+
+        // Best match
+        System.out.println("Best match 2 3 4 " + bestMatch(2,3,4));
+        System.out.println("Best match 4 3 3 " + bestMatch(4,3,3));
+        System.out.println("Best match 3 8 4 " + bestMatch(3,8,4));
+        System.out.println("Best match 4 3 4 " + bestMatch(4,3,4));
+        System.out.println("Best match 2 4 4 " + bestMatch(2,4,4));
+        System.out.println("Best match 8 8 4 " + bestMatch(8,8,4));
+        System.out.println("Best match 4 4 4 " + bestMatch(4,4,4));
+
+        // Best Fit
+        System.out.println("Find Best fit 2 3 6 is " + findBestFit(2,3,6));
+        System.out.println("Find Best fit 4 3 6 is " + findBestFit(4,3,6));
+        System.out.println("Find Best fit 3 4 6 is " + findBestFit(3,4,6));
+        System.out.println("Find Best fit 2 3 1 is " + findBestFit(2,3,1));
+        System.out.println("Find Best fit 6 3 4 is " + findBestFit(6,3,4));
+        System.out.println("Find Best fit 3 6 4 is " + findBestFit(3,6,4));
+
+
+
+
+
+
+
+
+
+    }
+    }
+
+
+
+
+
